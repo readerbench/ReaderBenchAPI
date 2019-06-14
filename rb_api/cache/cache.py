dd264a231c8f36a87f5ea219d9b7fa4e86a6b98f
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2019-06-14 13:28:00 +0300

    Fixed another minor bug on caching

--- rb_api/cache/cache.py
+++ rb_api/cache/cache.py
@@ -5,7 +5,7 @@ from rb.core.lang import Lang
 cache = MemcachedCache()
 
 def get_model(vtype: VectorModelType, name: str, lang: Lang, size: int = 300):
-    model_name = vtype.name + "_" + name + "_" + lang.value + "_" + size
+    model_name = "{}_{}_{}_{}".format(vtype.name, name, lang.value, size)
     global cache
     model = cache.get(model_name)
     if model is None:
