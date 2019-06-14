809986466a0b468098306a27aa231c4445c2a6ef
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2019-06-14 13:15:55 +0300

    Included caching in AMoC

--- rb_api/amoc/amoc.py
+++ rb_api/amoc/amoc.py
@@ -2,10 +2,12 @@ from flask import Flask, request
 import json
 from rb.core.lang import Lang
 from rb_api.amoc.comprehension_model_service import ComprehensionModelService
+from rb.similarity.vector_model import VectorModelType
 from rb.similarity.word2vec import Word2Vec
 from rb.similarity.lsa import LSA
 from rb.similarity.lda import LDA
 from rb_api.dto.amoc.amoc_response import AmocResponse
+import rb_api.cache.cache as cache
 
 app = Flask(__name__)
 
@@ -22,7 +24,10 @@ def amocPost():
     max_active_concepts = int(params.get("maxActiveConcepts"))
     max_semantic_expand = int(params.get("maxSemanticExpand"))
 
-    semantic_models = [Word2Vec(semantic_model, Lang.EN), LDA(semantic_model, Lang.EN), LSA(semantic_model, Lang.EN)]
+    w2v = cache.get_model(VectorModelType.WORD2VEC, semantic_model, Lang.EN)
+    lda = cache.get_model(VectorModelType.LDA, semantic_model, Lang.EN)
+    lsa = cache.get_model(VectorModelType.LSA, semantic_model, Lang.EN)
+    semantic_models = [w2v, lda, lsa]
     cms = ComprehensionModelService(semantic_models, Lang.EN,
                                     min_activation_threshold, max_active_concepts, max_semantic_expand)
 
