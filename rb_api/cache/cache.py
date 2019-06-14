492c352a4eeed6cf1783f1ae933aedc1610603f6
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2019-06-14 13:35:11 +0300

    Fixed word2vec loading problems

--- rb_api/cache/cache.py
+++ rb_api/cache/cache.py
@@ -1,15 +1,20 @@
 from werkzeug.contrib.cache import MemcachedCache
 from rb.similarity.vector_model import VectorModelType, VectorModel
+from rb.similarity.word2vec import Word2Vec
 from rb.core.lang import Lang
 
 cache = MemcachedCache()
 
-def get_model(vtype: VectorModelType, name: str, lang: Lang, size: int = 300):
+
+def get_model(vtype: VectorModelType, name: str, lang: Lang, size: int = 300) -> VectorModel:
     model_name = "{}_{}_{}_{}".format(vtype.name, name, lang.value, size)
     global cache
     model = cache.get(model_name)
     if model is None:
-        model = VectorModel(vtype, name, lang, size)
+        if vtype == VectorModelType.WORD2VEC:
+            model = Word2Vec(name, lang, size)
+        else:
+            model = VectorModel(vtype, name, lang, size)
         cache.set(model_name, model)
     return model
 
